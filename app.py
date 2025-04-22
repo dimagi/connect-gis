9be41cbdd184def21610da96c3d28ffd2dd2ce53
thushar <thushar.work@gmail.com>
2025-04-22 11:25:38 +0530

    update port to 5010

--- app.py
+++ app.py
@@ -507,4 +507,4 @@ def getReports_sqlQuery():
 
 # Run the Flask app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5010, debug=True)
